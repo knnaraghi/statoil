1c5e254d770898dbd15a10903e965dcee504a181
Khashayar Naraghi <knnaraghi@gmail.com>
2018-01-30 01:41:43 -0500

    transfer_model changes
    Changed block of if statements to lambda usage
    
    Added basic CNN to models

--- transfer_model.py
+++ transfer_model.py
@@ -2,33 +2,30 @@ from keras.layers import GlobalMaxPooling2D, Dropout, Dense, Input
 from keras.layers import concatenate
 from keras.models import Model
 from keras.optimizers import SGD
-from keras.applications.vgg16 import VGG16, VGG19
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
 from keras.applications.xception import Xception 
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.resnet50 import ResNet50
-
+from basic_cnn import cnn_model as basic_cnn
 
 def transferModel(transfer_model):
     ang_input = Input(shape=(1,))
     angle_layer = Dense(1,)(ang_input)
     
-    if transfer_model == 'vgg16':
-        base_model = VGG16(weights='imagenet', include_top=False, input_shape=(75, 75, 3), classes=1)
+    models = {'vgg16': lambda: VGG16(weights='imagenet', include_top=False, input_shape=(75, 75, 3), classes=1),
+              'vgg19': lambda: VGG19(weights='imagenet', include_top=False, input_shape=(75, 75, 3), classes=1),
+              'Xception': lambda: Xception(weights='imagenet', include_top=False, input_shape=(75, 75, 3), classes=1),
+              'InceptionV3': lambda: InceptionV3(weights='imagenet', include_top=False, input_shape=(139, 139, 3), classes=1),
+              'Resnet50': lambda: ResNet50(weights='imagenet', include_top=False, input_shape=(200, 200, 3), classes=1),
+              'Basic_CNN': lambda: basic_cnn()
+    }
     
-    if transfer_model == 'vgg19':
-        base_model = VGG19(weights='imagenet', include_top=False, input_shape=(75, 75, 3), classes=1)
-    
-    if transfer_model == 'Xception':
-        base_model = Xception(weights='imagenet', include_top=False, input_shape=(75, 75, 3), classes=1)
+    base_model = models[transfer_model]()
         
-    if transfer_model == 'InceptionV3':
-        base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(139, 139, 3), classes=1)
-        
-    if transfer_model == 'Resnet50':
-        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(200, 200, 3), classes=1)
-        
-    for layer in base_model.layers:
-        layer.trainble = False
+    if transfer_model != 'Basic_CNN':
+        for layer in base_model.layers :
+            layer.trainble = False
         
     transferModel = base_model.output
     
@@ -41,7 +38,7 @@ def transferModel(transfer_model):
     
     predictions = Dense(1, activation='sigmoid')(transferModel)
     
-    model = Model([base_model.input, ang_input], output=predictions)
+    model = Model(inputs=[base_model.input, ang_input], outputs=predictions)
     
     optimizer = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     
